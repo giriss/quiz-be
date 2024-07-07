11751a372383e2da8d009166f6f1f12be4ec1c71
Girish Gopaul <girish@gopaul.me>
2024-07-07 11:23:20 +0400

    Added cors

--- app/main.py
+++ app/main.py
@@ -1,5 +1,22 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from .routers import router
 
 app = FastAPI()
+
+origins = [
+    "http://localhost:5173",
+    "http://localhost:4173",
+    "https://quiz-fe.gopaul.me",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+    expose_headers=["x-token"],
+)
+
 app.include_router(router)
